eac8c229af0d03bdf3fb391ddbb2a81a1b2a97e3
jeongsooyoung <code.ameba@gmail.com>
2020-03-14 19:52:31 +0900

    modified colon

--- clock.js
+++ clock.js
@@ -7,8 +7,8 @@ function getTime() {
     const minutes = date.getMinutes();
     const seconds = date.getSeconds();
     clockTitle.innerHTML = 
-    `${hours < 10 ? `0${hours}` : hours}:
-    ${minutes < 10 ? `0${minutes}` : minutes}:
+    `${hours < 10 ? `0${hours}` : hours} :
+    ${minutes < 10 ? `0${minutes}` : minutes} :
     ${seconds < 10 ? `0${seconds}` : seconds}`;
 }
 
